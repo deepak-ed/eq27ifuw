a041aa3757fd80def53948695eb347b782275414
deepak-ed <deepaked99@gmail.com>
2021-11-13 23:56:13 +0100

    changes

--- eq27ifuw/function.py
+++ eq27ifuw/function.py
@@ -1,5 +1,14 @@
+import matplotlib.pyplot as plt
 import numpy as np
 from ipywidgets import interact, fixed
 from PIL import Image
+
 def imshow(X, resize=None):
-  Y = np.resize(
+  im = Image.fromarray(X)
+  im1 = im.resize(resize)
+  plt.figure()
+  plt.imshow(im1)
+  plt.show()
+  return 0
+
+#interact(imshow, resize=[(32,32),(12,12)] )
