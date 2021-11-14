7ddedd2bf55a68ec19007200d71df8ebfb90adc8
deepak-ed <deepaked99@gmail.com>
2021-11-14 01:02:54 +0100

    changes

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from setuptools import find_packages
 setup(name='eq27ifuw',
 version='0.1',
 author='DSSS',
-author_email='deepak.bhatia@fau.de',
+author_email='deepakbhatia1999@gmail.com',
 packages=find_packages(),
 install_requires=['numpy', 'Pillow', 'ipywidgets'])
