49b04b9f3dfe7623bb3acc97e8d519fa76ba747e
deepak-ed <deepaked99@gmail.com>
2021-11-14 18:38:17 +0100

    changes

--- eq27ifuw/function.py
+++ eq27ifuw/function.py
@@ -3,7 +3,6 @@ import numpy as np
 from ipywidgets import interact, fixed
 from PIL import Image
 
-@interact(X=fixed('Image') , resize = 10 )
 
 def imshow(X, resize=None):
   im = Image.fromarray(X)
@@ -11,6 +10,8 @@ def imshow(X, resize=None):
   plt.figure()
   plt.imshow(im1)
   plt.show()
-  return 0
+
+
+interact(imshow , resize = 10 )
 
 
