fedb50cd464d02ae4209b5923b3e1b847be49f51
deepak-ed <deepaked99@gmail.com>
2021-11-14 18:02:39 +0100

    changes

--- eq27ifuw/function.py
+++ eq27ifuw/function.py
@@ -3,12 +3,14 @@ import numpy as np
 from ipywidgets import interact, fixed
 from PIL import Image
 
+@interact(X=fixed('Image') , resize = 10 )
+
 def imshow(X, resize=None):
   im = Image.fromarray(X)
-  im1 = im.resize(resize)
+  im1 = im.resize((resize, resize))
   plt.figure()
   plt.imshow(im1)
   plt.show()
   return 0
 
-@interact(X=fixed('Image') , resize =widgets.IntSlider(min=10, max=1250))
+
